e440cf22ad909a96db8380bf335ed26652feda46
Ben Langmead <ben.langmead@gmail.com>
2019-04-23 09:28:48 -0400

    add readme and MARCC copy script

---
+++ README.md
@@ -0,0 +1,6 @@
+Requires:
+
+* [bcftools](https://samtools.github.io/bcftools/) ([conda](https://anaconda.org/bioconda/bcftools))
+* [vcftools](https://vcftools.github.io/perl_module.html) ([conda](https://anaconda.org/bioconda/vcftools))
+* [samtools](https://samtools.github.io) ([conda](https://anaconda.org/bioconda/samtools))
+* [tabix](http://www.htslib.org/doc/tabix.html) ([conda](https://anaconda.org/bioconda/tabix))
---
+++ cp.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -ex
+
+BASE=/scratch/groups/blangme2/naechyun/major_allele
+
+mkdir -p scripts
+cp ${BASE}/scripts/build_major_allele_ref.sh scripts/
+cp ${BASE}/scripts/convert_header_v43_to_v42.sh scripts/
+cp ${BASE}/scripts/replace_fa_seq.py scripts/
+cp ${BASE}/scripts/test_major_allele_ref.py scripts/
+cp ${BASE}/scripts/test_major_allele_ref.sh scripts/
+cp ${BASE}/scripts/vcf_processing.py scripts/
+
+mkdir -p hg19
+cp ${BASE}/grch37/build37.sh hg19/
+cp ${BASE}/grch37/test37.sh hg19/
+
+mkdir -p grch38
+cp ${BASE}/grch38/build38.sh grch38/
+cp ${BASE}/grch38/test38.sh grch38/
+
