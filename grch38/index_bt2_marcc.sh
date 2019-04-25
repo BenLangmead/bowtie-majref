bc9d050588a2151b80c0103c5793749eebf883cc
Ben Langmead <ben.langmead@gmail.com>
2019-04-25 17:45:36 -0400

    nudge time limit

--- grch38/index_bt2_marcc.sh
+++ grch38/index_bt2_marcc.sh
@@ -5,7 +5,7 @@
 #SBATCH --mem=16G
 #SBATCH --output=.index_bt2.out
 #SBATCH --error=.index_bt2.err
-#SBATCH --time=4:00:00
+#SBATCH --time=5:00:00
 #SBATCH --ntasks-per-node=4
 
 bowtie2-build h38_1kgmaj.fa grch38_1kgmaj
