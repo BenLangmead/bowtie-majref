9b623d13e6165e34acc09e30f5f3f1ee5f54e3b5
Ben Langmead <ben.langmead@gmail.com>
2019-04-25 07:26:43 -0400

    bump memory/time

--- hg19/index_bt2_marcc.sh
+++ hg19/index_bt2_marcc.sh
@@ -2,10 +2,10 @@
 #SBATCH
 #SBATCH --partition=shared
 #SBATCH --nodes=1
-#SBATCH --mem=12G
+#SBATCH --mem=16G
 #SBATCH --output=.index_bt2.out
-#SBATCH --error=.index_bt2.er
-#SBATCH --time=4:00:00
+#SBATCH --error=.index_bt2.err
+#SBATCH --time=5:00:00
 #SBATCH --ntasks-per-node=4
 
 bowtie2-build h37_1kgmaj.fa hg19_1kgmaj
--- hg19/index_bt_marcc.sh
+++ hg19/index_bt_marcc.sh
@@ -2,10 +2,10 @@
 #SBATCH
 #SBATCH --partition=shared
 #SBATCH --nodes=1
-#SBATCH --mem=12G
+#SBATCH --mem=16G
 #SBATCH --output=.index_bt.out
-#SBATCH --error=.index_bt.er
-#SBATCH --time=4:00:00
+#SBATCH --error=.index_bt.err
+#SBATCH --time=5:00:00
 #SBATCH --ntasks-per-node=4
 
 bowtie-build h37_1kgmaj.fa hg19_1kgmaj
