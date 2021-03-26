577e2f194f4c97c4c9b4ffb75f88a1d490d87dc2
milkschen <s93rm6@gmail.com>
2021-03-26 09:30:58 -0400

    updated to new vcf_proc interface

--- grch38/test38.sh
+++ grch38/test38.sh
@@ -6,7 +6,7 @@ REF="GCA_000001405.15_GRCh38_no_alt_analysis_set.fna"
 if [[ -f wgs.concat.vcf.gz ]]; then
     VCF="wgs.concat.vcf.gz"
     bgzip -cd ${VCF} | \
-        python ../scripts/vcf_processing.py --out_var_loc 1 --min_af 0.5 --max_allele_len 1 | \
+        python ../scripts/vcf_processing.py --out_var_loc --min_af 0.5 --max_allele_len 1 | \
         python ../scripts/test_major_allele_ref.py -m ${MAJ} \
         -r ${REF} > test_h38.txt
 else
@@ -14,20 +14,20 @@ else
     VCF_X="ALL.chrX_GRCh38_sites.20170504.vcf.gz"
     VCF_Y="ALL.chrY_GRCh38_sites.20170504.vcf.gz"
     bgzip -cd ${VCF} | \
-        python ../scripts/vcf_processing.py --out_var_loc 1 --min_af 0.5 --max_allele_len 1 | \
+        python ../scripts/vcf_processing.py --out_var_loc --min_af 0.5 --max_allele_len 1 | \
         python ../scripts/test_major_allele_ref.py -m ${MAJ} \
         -r ${REF} > test_h38.txt
 
     bgzip -cd ${VCF_Y} | \
-        python ../scripts/vcf_processing.py --out_var_loc 1 --min_af 0.5 --max_allele_len 1 | \
+        python ../scripts/vcf_processing.py --out_var_loc --min_af 0.5 --max_allele_len 1 | \
         python ../scripts/test_major_allele_ref.py -m ${MAJ} \
         -r ${REF} -t chrY > test_h38_y.txt
 
     bgzip -cd ${VCF} | \
         grep 'chrX' | \
-        python ../scripts/vcf_processing.py --out_var_loc 1 --min_af 0.5 --max_allele_len 1 > var_x_h38.txt
+        python ../scripts/vcf_processing.py --out_var_loc --min_af 0.5 --max_allele_len 1 > var_x_h38.txt
     bgzip -cd ${VCF_X} | \
-        python ../scripts/vcf_processing.py --out_var_loc 1 --min_af 0.5 --max_allele_len 1 >> var_x_h38.txt
+        python ../scripts/vcf_processing.py --out_var_loc --min_af 0.5 --max_allele_len 1 >> var_x_h38.txt
     python ../scripts/test_major_allele_ref.py -m ${MAJ} \
         -r ${REF} -t chrX -c var_x_h38.txt > test_h38_x.txt
 fi
