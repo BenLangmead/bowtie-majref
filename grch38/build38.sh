4158cb033c656d4892b5fb736067a7814dd2f6ea
milkschen <s93rm6@gmail.com>
2021-03-26 05:40:52 -0400

    update the pipeline; now only use bcftools; now use the latest 1kgp calls

--- grch38/build38.sh
+++ grch38/build38.sh
@@ -3,33 +3,73 @@
 set -ex
 
 # 1. Download hg38 genome
-wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/001/405/GCA_000001405.15_GRCh38/seqs_for_alignment_pipelines.ucsc_ids/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz
-bgzip -d GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz
+if [[ ! -f GCA_000001405.15_GRCh38_no_alt_analysis_set.fna ]]; then
+    wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/001/405/GCA_000001405.15_GRCh38/seqs_for_alignment_pipelines.ucsc_ids/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz
+    bgzip -d GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz
+fi
+
+# GRCh38 p13
+# if [[ -z GCF_000001405.39_GRCh38.p13_genomic.fna ]]; then
+#     https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.39_GRCh38.p13/GCF_000001405.39_GRCh38.p13_genomic.fna.gz
+#     bgzip -d GCF_000001405.39_GRCh38.p13_genomic.fna.gz
+# fi
 
 # 2. Download WGS vcf from 1000 Genomes Project
-wget http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/data_collections/1000_genomes_project/release/20181203_biallelic_SNV/ALL.wgs.shapeit2_integrated_v1a.GRCh38.20181129.sites.vcf.gz
+if [[ ! -f ALL.wgs.shapeit2_integrated_snvindels_v2a.GRCh38.27022019.sites.vcf.gz ]]; then
+    wget http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/data_collections/1000_genomes_project/release/20190312_biallelic_SNV_and_INDEL/ALL.wgs.shapeit2_integrated_snvindels_v2a.GRCh38.27022019.sites.vcf.gz
+fi
+# Convert contig namings from "1" to "chr1"
+if [[ ! -f ALL.wgs.shapeit2_integrated_snvindels_v2a.GRCh38.27022019.sites.renamed.vcf.gz ]]; then 
+    bcftools annotate --rename-chrs GRCh38.chrom_map -O z -o ALL.wgs.shapeit2_integrated_snvindels_v2a.GRCh38.27022019.sites.renamed.vcf.gz ALL.wgs.shapeit2_integrated_snvindels_v2a.GRCh38.27022019.sites.vcf.gz
+fi
 
-# 3. Convert the header from v4.3 to v4.2
-sh ../scripts/convert_header_v43_to_v42.sh ALL.wgs.shapeit2_integrated_v1a.GRCh38.20181129.sites
+filter_major_snp() {
+    # Get major-alleles (SNVs) from a VCF.
+    # $1: the prefix of a VCF.GZ file
+    OUT="${1}.major.vcf.gz"
+    bcftools view -O z -m 2 -M 2 -v snps -q 0.5 -e 'AF = 0.5' -o ${OUT} ${1}.vcf.gz
+    bcftools index ${OUT}
+}
 
-# 4. Build major allele ref (no chrY)
-sh ../scripts/build_major_allele_ref.sh h38_1kgmaj_no_y ALL.wgs.shapeit2_integrated_v1a.GRCh38.20181129.sites_v42.vcf.gz GCA_000001405.15_GRCh38_no_alt_analysis_set.fna
+filter_major_snp_indel() {
+    # Get major-alleles (SNVs and indels) from a VCF.
+    # $1: the prefix of a VCF.GZ file
+    OUT="${1}.major.vcf.gz"
+    bcftools view -O z -m 2 -M 2 -v snps,indels -q 0.5 -e 'AF = 0.5' -o ${OUT} ${1}.vcf.gz
+    bcftools index ${OUT}
+}
 
-# 5. Prepare chrY genome
-mkdir h38_liftover
-cd h38_liftover
-samtools faidx ../GCA_000001405.15_GRCh38_no_alt_analysis_set.fna chrY > chrY.fa
-mv ../h38_1kgmaj_no_y.fa .
+# We used the "lift-over" calls in the non-pseudo-autosomal regions since they were not included
+# in the GRCh38-based 1KGP calls we used.
+# 
+# See:
+#   Lowy-Gallego E, Fairley S, Zheng-Bradley X et al. Variant calling on the GRCh38 assembly 
+#   with the data from phase three of the 1000 Genomes Project [version 2; peer review: 2 approved]. 
+#   Wellcome Open Res 2019, 4:50 (https://doi.org/10.12688/wellcomeopenres.15126.2)
+# "Variants on the Y chromosome or in regions of the X chromosome outside the pseudo-autosomal 
+# regions were discarded due to the ploidy settings used in this work."
 
-# 6. Download chrY vcf from 1000 Genomes Project (GRCh38-liftover)
-wget http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/supporting/GRCh38_positions/ALL.chrY_GRCh38_sites.20170504.vcf.gz
-wget http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/supporting/GRCh38_positions/ALL.chrX_GRCh38_sites.20170504.vcf.gz
+if [[ ! -f ALL.chrY_GRCh38_sites.20170504.vcf.gz ]]; then
+    wget http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/supporting/GRCh38_positions/ALL.chrY_GRCh38_sites.20170504.vcf.gz
+fi
+if [[ ! -f ALL.chrY_GRCh38_sites.20170504.renamed.vcf.gz ]]; then 
+    bcftools annotate --rename-chrs GRCh38.chrom_map -O z -o ALL.chrY_GRCh38_sites.20170504.renamed.vcf.gz ALL.chrY_GRCh38_sites.20170504.vcf.gz
+fi
+if [[ ! -f ALL.chrX_GRCh38_sites.20170504.vcf.gz ]]; then
+    wget http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/supporting/GRCh38_positions/ALL.chrX_GRCh38_sites.20170504.vcf.gz
+fi
+if [[ ! -f ALL.chrX_GRCh38_sites.20170504.renamed.vcf.gz ]]; then 
+    bcftools annotate --rename-chrs GRCh38.chrom_map -O z -o ALL.chrX_GRCh38_sites.20170504.renamed.vcf.gz ALL.chrX_GRCh38_sites.20170504.vcf.gz
+fi
 
-# 7. build major allele ref
-sh ../../scripts/build_major_allele_ref.sh h38_1kgmaj_y ALL.chrY_GRCh38_sites.20170504.vcf.gz chrY.fa
-sh ../../scripts/build_major_allele_ref.sh h38_1kgmaj_no_y_update_x ALL.chrX_GRCh38_sites.20170504.vcf.gz h38_1kgmaj_no_y.fa
+if [[ ! -f wgs.concat.vcf.gz ]]; then
+    bcftools concat -O z -o wgs.concat.unsorted.vcf.gz ALL.wgs.shapeit2_integrated_snvindels_v2a.GRCh38.27022019.sites.renamed.vcf.gz ALL.chrX_GRCh38_sites.20170504.renamed.vcf.gz ALL.chrY_GRCh38_sites.20170504.renamed.vcf.gz
+    bcftools sort -O z -o wgs.concat.vcf.gz wgs.concat.unsorted.vcf.gz
+fi
 
-# 8. Merge two genomes
-cd ..
-mv h38_liftover/h38_1kgmaj_no_y_update_x.fa .
-python ../scripts/replace_fa_seq.py -f h38_1kgmaj_no_y_update_x.fa -s chrY -n h38_liftover/h38_1kgmaj_y.fa > h38_1kgmaj.fa
+# Obtain major-alleles. Output: wgs.concat.major.vcf.gz
+filter_major_snp wgs.concat
+
+# Build the major-alelle reference
+bcftools consensus -f GCA_000001405.15_GRCh38_no_alt_analysis_set.fna -o grch38_1kgmaj.fa wgs.concat.major.vcf.gz
+
--- grch38/test38.sh
+++ grch38/test38.sh
@@ -1,17 +1,33 @@
-bgzip -cd ALL.wgs.shapeit2_integrated_v1a.GRCh38.20181129.sites.vcf.gz | \
-    python ../scripts/vcf_processing.py --out_var_loc 1 --min_af 0.5 --max_allele_len 1 | \
-    python ../scripts/test_major_allele_ref.py -m h38_1kgmaj.fa \
-    -r GCA_000001405.15_GRCh38_no_alt_analysis_set.fna > test_h38.txt
+set -ex
 
-bgzip -cd h38_liftover/ALL.chrY_GRCh38_sites.20170504.vcf.gz | \
-    python ../scripts/vcf_processing.py --out_var_loc 1 --min_af 0.5 --max_allele_len 1 | \
-    python ../scripts/test_major_allele_ref.py -m h38_1kgmaj.fa \
-    -r GCA_000001405.15_GRCh38_no_alt_analysis_set.fna -t chrY > test_h38_y.txt
+MAJ="grch38_1kgmaj.fa"
+REF="GCA_000001405.15_GRCh38_no_alt_analysis_set.fna"
 
-bgzip -cd ALL.wgs.shapeit2_integrated_v1a.GRCh38.20181129.sites_v42.vcf.gz | \
-    grep 'chrX' | \
-    python ../scripts/vcf_processing.py --out_var_loc 1 --min_af 0.5 --max_allele_len 1 > var_x_h38.txt
-bgzip -cd h38_liftover/ALL.chrX_GRCh38_sites.20170504.vcf.gz | \
-    python ../scripts/vcf_processing.py --out_var_loc 1 --min_af 0.5 --max_allele_len 1 >> var_x_h38.txt
-python ../scripts/test_major_allele_ref.py -m h38_1kgmaj.fa \
-    -r GCA_000001405.15_GRCh38_no_alt_analysis_set.fna -t chrX -c var_x_h38.txt > test_h38_x.txt
+if [[ -f wgs.concat.vcf.gz ]]; then
+    VCF="wgs.concat.vcf.gz"
+    bgzip -cd ${VCF} | \
+        python ../scripts/vcf_processing.py --out_var_loc 1 --min_af 0.5 --max_allele_len 1 | \
+        python ../scripts/test_major_allele_ref.py -m ${MAJ} \
+        -r ${REF} > test_h38.txt
+else
+    VCF="ALL.wgs.shapeit2_integrated_snvindels_v2a.GRCh38.27022019.sites.renamed.vcf.gz"
+    VCF_X="ALL.chrX_GRCh38_sites.20170504.vcf.gz"
+    VCF_Y="ALL.chrY_GRCh38_sites.20170504.vcf.gz"
+    bgzip -cd ${VCF} | \
+        python ../scripts/vcf_processing.py --out_var_loc 1 --min_af 0.5 --max_allele_len 1 | \
+        python ../scripts/test_major_allele_ref.py -m ${MAJ} \
+        -r ${REF} > test_h38.txt
+
+    bgzip -cd ${VCF_Y} | \
+        python ../scripts/vcf_processing.py --out_var_loc 1 --min_af 0.5 --max_allele_len 1 | \
+        python ../scripts/test_major_allele_ref.py -m ${MAJ} \
+        -r ${REF} -t chrY > test_h38_y.txt
+
+    bgzip -cd ${VCF} | \
+        grep 'chrX' | \
+        python ../scripts/vcf_processing.py --out_var_loc 1 --min_af 0.5 --max_allele_len 1 > var_x_h38.txt
+    bgzip -cd ${VCF_X} | \
+        python ../scripts/vcf_processing.py --out_var_loc 1 --min_af 0.5 --max_allele_len 1 >> var_x_h38.txt
+    python ../scripts/test_major_allele_ref.py -m ${MAJ} \
+        -r ${REF} -t chrX -c var_x_h38.txt > test_h38_x.txt
+fi
