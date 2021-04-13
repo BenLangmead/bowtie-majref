f668284bb570e578775f45eee5424585e13db8b8
milkschen <s93rm6@gmail.com>
2021-04-13 06:50:43 -0400

    do both snv and snv_indel

--- grch38/build38.sh
+++ grch38/build38.sh
@@ -14,7 +14,7 @@ fi
 #     bgzip -d GCF_000001405.39_GRCh38.p13_genomic.fna.gz
 # fi
 
-# 2. Download WGS vcf from 1000 Genomes Project
+# 2. Download WGS VCFs from 1000 Genomes Project
 if [[ ! -f ALL.wgs.shapeit2_integrated_snvindels_v2a.GRCh38.27022019.sites.vcf.gz ]]; then
     wget http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/data_collections/1000_genomes_project/release/20190312_biallelic_SNV_and_INDEL/ALL.wgs.shapeit2_integrated_snvindels_v2a.GRCh38.27022019.sites.vcf.gz
 fi
@@ -23,31 +23,15 @@ if [[ ! -f ALL.wgs.shapeit2_integrated_snvindels_v2a.GRCh38.27022019.sites.renam
     bcftools annotate --rename-chrs GRCh38.chrom_map -O z -o ALL.wgs.shapeit2_integrated_snvindels_v2a.GRCh38.27022019.sites.renamed.vcf.gz ALL.wgs.shapeit2_integrated_snvindels_v2a.GRCh38.27022019.sites.vcf.gz
 fi
 
-filter_major_snp() {
-    # Get major-alleles (SNVs) from a VCF.
-    # $1: the prefix of a VCF.GZ file
-    OUT="${1}.major.vcf.gz"
-    bcftools view -O z -m 2 -M 2 -v snps -q 0.5 -e 'AF = 0.5' -o ${OUT} ${1}.vcf.gz
-    bcftools index ${OUT}
-}
-
-filter_major_snp_indel() {
-    # Get major-alleles (SNVs and indels) from a VCF.
-    # $1: the prefix of a VCF.GZ file
-    OUT="${1}.major.vcf.gz"
-    bcftools view -O z -m 2 -M 2 -v snps,indels -q 0.5 -e 'AF = 0.5' -o ${OUT} ${1}.vcf.gz
-    bcftools index ${OUT}
-}
-
-# We used the "lift-over" calls in the non-pseudo-autosomal regions since they were not included
-# in the GRCh38-based 1KGP calls we used.
+# We use the "lift-over" calls in the non-pseudo-autosomal regions since they are not included
+# in the GRCh38-based 1KGP calls we use.
 # 
 # See:
-#   Lowy-Gallego E, Fairley S, Zheng-Bradley X et al. Variant calling on the GRCh38 assembly 
-#   with the data from phase three of the 1000 Genomes Project [version 2; peer review: 2 approved]. 
-#   Wellcome Open Res 2019, 4:50 (https://doi.org/10.12688/wellcomeopenres.15126.2)
 # "Variants on the Y chromosome or in regions of the X chromosome outside the pseudo-autosomal 
-# regions were discarded due to the ploidy settings used in this work."
+#  regions were discarded due to the ploidy settings used in this work."
+#       Lowy-Gallego E, Fairley S, Zheng-Bradley X et al. Variant calling on the GRCh38 assembly 
+#       with the data from phase three of the 1000 Genomes Project. 
+#       Wellcome Open Res 2019, 4:50 (https://doi.org/10.12688/wellcomeopenres.15126.2)
 
 if [[ ! -f ALL.chrY_GRCh38_sites.20170504.vcf.gz ]]; then
     wget http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/supporting/GRCh38_positions/ALL.chrY_GRCh38_sites.20170504.vcf.gz
@@ -67,9 +51,40 @@ if [[ ! -f wgs.concat.vcf.gz ]]; then
     bcftools sort -O z -o wgs.concat.vcf.gz wgs.concat.unsorted.vcf.gz
 fi
 
-# Obtain major-alleles. Output: wgs.concat.major.vcf.gz
-filter_major_snp wgs.concat
+# 3. Extract major alleles
+filter_major_snv() {
+    # Get major-alleles (SNVs) from a VCF.
+    # $1: path to the input VCF file
+    # $2: path to the output VCF file
+    IN=$1
+    OUT=$2
+    bcftools view -O z -m 2 -M 2 -v snps -q 0.5 -e 'AF = 0.5' -o ${OUT} ${IN}
+    bcftools index ${OUT}
+}
 
-# Build the major-alelle reference
-bcftools consensus -f GCA_000001405.15_GRCh38_no_alt_analysis_set.fna -o grch38_1kgmaj.fa wgs.concat.major.vcf.gz
+filter_major_snv_indel() {
+    # Get major-alleles (SNVs and indels) from a VCF.
+    # $1: path to the input VCF file
+    # $2: path to the output VCF file
+    IN=$1
+    OUT=$2
+    bcftools view -O z -m 2 -M 2 -v snps,indels -q 0.5 -e 'AF = 0.5' -o ${OUT} ${IN}
+    bcftools index ${OUT}
+}
+
+# Obtain major-alleles. Output: wgs.concat.major_snvs.vcf.gz or wgs.concat.major_snvindels.vcf.gz
+if [[ ! -f wgs.concat.major_snvs.vcf.gz ]]; then
+    filter_major_snv wgs.concat.vcf.gz wgs.concat.major_snvs.vcf.gz
+fi
+if [[ ! -f wgs.concat.major_snvindels.vcf.gz ]]; then
+    filter_major_snv_indel wgs.concat.vcf.gz wgs.concat.major_snvindels.vcf.gz
+fi
+
+# 4. Build the major-alelle reference
+if [[ ! -f grch38_1kgmaj_snvs.fa ]]; then
+    bcftools consensus -f GCA_000001405.15_GRCh38_no_alt_analysis_set.fna -o grch38_1kgmaj_snvs.fa wgs.concat.major_snvs.vcf.gz
+fi
+if [[ ! -f grch38_1kgmaj_snvindels.fa ]]; then
+    bcftools consensus -f GCA_000001405.15_GRCh38_no_alt_analysis_set.fna -o grch38_1kgmaj_snvindels.fa wgs.concat.major_snvindels.vcf.gz
+fi
 
