f6aff1879d4b63b64b737768de7f7a4655a86693
milkschen <s93rm6@gmail.com>
2021-04-14 11:22:33 -0400

    snv & snv+indel; built leviosam index for snv+indel

--- hg19/zip_bt2.sh
+++ hg19/zip_bt2.sh
@@ -1,14 +1,31 @@
 #!/bin/sh
 
-BASE=hg19_1kgmaj
-EXT=bt2
+BASE="hg19_1kgmaj_snvs"
+EXT="bt2"
 
-zip ${BASE}_bt2.zip \
+zip ${BASE}_${EXT}.zip \
     ${BASE}.1.${EXT} \
     ${BASE}.2.${EXT} \
     ${BASE}.3.${EXT} \
     ${BASE}.4.${EXT} \
     ${BASE}.rev.1.${EXT} \
     ${BASE}.rev.2.${EXT} \
+    wgs.concat.major_snvs.vcf.gz \
+    wgs.concat.major_snvs.vcf.gz.csi \
+    README.md
+
+BASE="hg19_1kgmaj_snvindels"
+EXT="bt2"
+
+zip ${BASE}_${EXT}.zip \
+    ${BASE}.1.${EXT} \
+    ${BASE}.2.${EXT} \
+    ${BASE}.3.${EXT} \
+    ${BASE}.4.${EXT} \
+    ${BASE}.rev.1.${EXT} \
+    ${BASE}.rev.2.${EXT} \
+    ${BASE}.lft \
+    wgs.concat.major_snvindels.vcf.gz \
+    wgs.concat.major_snvindels.vcf.gz.csi \
     README.md
 
