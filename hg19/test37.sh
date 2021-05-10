1f406425d216fcd40993137348ce382b482ae477
milkschen <s93rm6@gmail.com>
2021-05-10 06:44:11 -0400

    updated ref name

--- hg19/test37.sh
+++ hg19/test37.sh
@@ -3,5 +3,5 @@ if [[ ! -f ALL.wgs.phase3_shapeit2_mvncall_integrated_v5b.20130502.sites.renamed
 fi
 bgzip -cd ALL.wgs.phase3_shapeit2_mvncall_integrated_v5b.20130502.sites.renamed.vcf.gz |\
     python ../scripts/vcf_processing.py --out_var_loc --min_af 0.5 --max_allele_len 1 | \
-    python ../scripts/test_major_allele_ref.py -m hg19_1kgmaj.fa \
+    python ../scripts/test_major_allele_ref.py -m hg19_1kgmaj_snvs.fa \
     -r hg19.fa > test_h37.txt
